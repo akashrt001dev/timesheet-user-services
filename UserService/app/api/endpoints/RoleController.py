de5ac5587babf4ecc9eeb763a140d3d609530479
unknown <akhildesigner1991@gmail.com>
2025-12-18 16:06:27 +0530

    fix get role api

--- UserService/app/api/endpoints/RoleController.py
+++ UserService/app/api/endpoints/RoleController.py
@@ -27,7 +27,7 @@ async def createNewRole(
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-@router.get("/")
+@router.get("")
 async def getRoles(
     # X_Authorization: str = Header(alias="X-Authorization"),
     roleType: Optional[List[str]] = Query(None, alias="roleType"),
